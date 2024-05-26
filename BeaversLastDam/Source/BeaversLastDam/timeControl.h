497304c562569789231fce6fdbe0b205bef8f2b9
Steven <32489499+Zeldame@users.noreply.github.com>
2024-05-26 18:06:53 +0100

    SO IT BEGINGS

---
+++ BeaversLastDam/Source/BeaversLastDam/timeControl.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class BEAVERSLASTDAM_API timeControl
+{
+public:
+	timeControl();
+	~timeControl();
+};
